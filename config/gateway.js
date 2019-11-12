282d2daeb29cdf4095583bd5d02cd4b21b0829aa
jasony62 <jasony62@qq.com>
2019-11-12 22:40:13 +0800

    支持记录api调用次数

--- config/gateway.js
+++ config/gateway.js
@@ -7,15 +7,16 @@ module.exports = {
   trace: {
     mongodb: { host: 'localhost', port: 27017, database: 'tms-api-gw' }
   },
+  quota: {
+    mongodb: { host: 'localhost', port: 27017, database: 'tms-api-gw' },
+    client: { default: 100 },
+    api: { default: 100 },
+    clientApi: { default: 100 }
+  },
   auth: {
     http: {
       url: 'http://localhost:3001/auth/client'
     }
   },
-  event: { redis: { host: '127.0.0.1', port: 6379 } },
-  quota: {
-    client: { default: 100 },
-    api: { default: 100 },
-    clientApi: { default: 100 }
-  }
+  queue: { redis: { host: '127.0.0.1', port: 6379 } }
 }
