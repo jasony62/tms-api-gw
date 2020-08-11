2abacc9508765b89aada7b136be9614001f73e09
liao1a <153887111@qq.com>
2020-08-11 11:36:38 +0800

    update

--- config/gateway.js
+++ config/gateway.js
@@ -20,7 +20,7 @@ module.exports = {
       password: process.env.TMS_TRACE_MONGODB_PASSWORD || false,
       host: process.env.TMS_TRACE_MONGODB_HOST || host,
       port: parseInt(process.env.TMS_TRACE_MONGODB_PORT) || 27017,
-      database: process.env.TMS_TRACE_MONGODB_DATABASE || 'tms-api-gw'
+      database: process.env.TMS_TRACE_MONGODB_DATABASE || 'tms-api-gw-jh'
     }
   },
   quota: {
@@ -30,12 +30,12 @@ module.exports = {
       password: process.env.TMS_QUOTA_MONGODB_PASSWORD || false,
       host: process.env.TMS_QUOTA_MONGODB_HOST || host,
       port: parseInt(process.env.TMS_QUOTA_MONGODB_PORT) || 27017,
-      database: process.env.TMS_QUOTA_MONGODB_DATABASE || 'tms-api-gw'
+      database: process.env.TMS_QUOTA_MONGODB_DATABASE || 'tms-api-gw-jh'
     },
     rules: {}
   },
   auth: {
-    enable: true,
+    enable: false,
     http: {
       query: process.env.TMS_AUTH_HTTP_QUERY ? JSON.parse(process.env.TMS_AUTH_HTTP_QUERY) : ['access_tocken', 'access_tocken'],
       url: process.env.TMS_AUTH_HTTP_URL || "http://localhost:3001/auth/token",
