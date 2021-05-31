2756513405b4665a47c32acaea1fe321a34a7360
ly A <153887111@qq.com>
2021-05-31 09:07:29 +0800

    0601 访问日志发送第三方之前对参数的优化处理

--- back/lib/trace/httpBeforeFun.js
+++ back/lib/trace/httpBeforeFun.js
@@ -1,4 +1,15 @@
+const _ = require("lodash")
+
 module.exports = function(req, event, clientId, headers = {}, data) {
-  if (req.clientObj) headers["x-request-client"] = process.env.YZ_CLIENTID ? req.clientObj[process.env.YZ_CLIENTID] : req.clientObj.data.custId
-  if (process.env.TMS_APP_NAME) headers["x-gateway-name"] = process.env.TMS_APP_NAME
+  if (req.clientObj) headers["x-request-client"] = process.env.YZ_CLIENTID ? _.get(req.clientObj, process.env.YZ_CLIENTID, "") : req.clientObj.data.custId
+
+  if (process.env.TMS_APP_NAME_URLPREFIX) {
+    const urlPrefix = JSON.parse(process.env.TMS_APP_NAME_URLPREFIX)
+    if (urlPrefix[req.urlPrefix]) {
+      headers["x-gateway-name"] = urlPrefix[req.urlPrefix]
+    }
+  }
+  if (!headers["x-gateway-name"] && process.env.TMS_APP_NAME) {
+    headers["x-gateway-name"] = process.env.TMS_APP_NAME
+  }
 }
